865470576cb773b7f71a4286880f8835e9a63211
Bishal292004 <bishalnath438@gmail.com>
2026-06-11 13:05:51 +0530

    the functionality of btn press and the flashing of the btn on press is added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let playerSeq = [];
 
 let level = 0;
 let gameStarted = false;
-let btnColor = ['red','blue', 'green', 'yellow'];
+let btnColor = ['red', 'blue', 'green', 'yellow'];
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener('keypress', function(){
     if(gameStarted == false){
@@ -21,8 +23,8 @@ function flashBtn(btn){
 }
 
 function levelUp(){
+    playerSeq= [];
     level++;
-    let h2 = document.querySelector("h2");
     h2.innerText = `Level ${level}`; 
     
     //now select a random button for flash
@@ -30,5 +32,28 @@ function levelUp(){
     let randBtnColor = btnColor[randIdx];
     let randBtn = document.querySelector(`.${randBtnColor}`);
 
+    gameSeq.push(randBtnColor);
+    console.log(gameSeq);
+
     flashBtn(randBtn);
 }
+
+
+
+function btnPress(){
+    let btn = this;
+    flashBtn(btn);
+
+    let userColor = btn.getAttribute("id");
+    playerSeq.push(userColor);
+    console.log(playerSeq);
+
+    checkAns(playerSeq.length-1);
+
+}
+
+let allbtn = document.querySelectorAll(".btn");
+for(btn of allbtn){
+    btn.addEventListener('click', btnPress);
+}
+
